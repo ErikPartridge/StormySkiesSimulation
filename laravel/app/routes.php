d43415b3b32cd41472c555a44b238b086a91cbb4
Erik Malmstrom-Partridge <erazor101@gmail.com>
2014-07-18 18:51:24 -0400

    finished preliminary routes

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -43,3 +43,51 @@ Route::post('/backend/used_aircraft', "AircraftController@usedPost");
 Route::get('/backend/new_aircraft', "AircraftController@newGet");
 
 Route::post('/backend/new_aircraft', "AircraftController@newPost");
+
+Route::get('/backend/routes', "RoutesController@mainGet");
+
+Route::post('/backend/routes', "RoutesController@mainPost");
+
+Route::get('/backend/create_route', "RoutesController@createGet");
+
+Route::post('/backend/create_route', "RoutesController@createPost");
+
+Route::get('/backend/edit_routes', "RoutesController@editGet");
+
+Route::post('/backend/edit_routes', "RoutesController@editPost");
+
+Route::get('/backend/research_route', "RoutesController@researchGet");
+
+Route::post('/backend/research_route', "RoutesController@researchPost");
+
+Route::get('/backend/airports', "AirportsController@mainGet");
+
+Route::post('/backend/airports', "AirportsController@mainPost");
+
+Route::get('/backend/gates', "AirportsController@gatesGet");
+
+Route::post('/backend/gates', "AirportsController@gatesPost");
+
+Route::get('/backend/slots', "AirportsController@slotsGet");
+
+Route::post('/backend/slots', "AirportsController@slotsPost");
+
+Route::get('/backend/global', "BackendController@overviewGet");
+
+Route::post('/backend/global', "BackendController@overviewPost");
+
+Route::get('/user/login', "UserController@getLogin");
+
+Route::post('/user/login', "UserController@postLogin");
+
+Route::get('/user/logoff', "UserController@getLogoff");
+
+Route::post('/user/logoff', "UserController@postLogoff");
+
+Route::get('/user/settings', "UserController@getSettings");
+
+Route::post('/user/settings', "UserController@postSettings");
+
+Route::get('/user/payment', "UserController@getPayment");
+
+Route::post('/user/payment', "UserController@postPayment");
