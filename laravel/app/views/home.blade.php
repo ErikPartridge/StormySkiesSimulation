d34b8aac9dd1aec6853802336d99c9add40dd9de
Erik Malmstrom-Partridge <erazor101@gmail.com>
2014-07-20 00:01:51 -0400

    styling

--- laravel/app/views/home.blade.php
+++ laravel/app/views/home.blade.php
@@ -4,25 +4,14 @@
     <meta charset="UTF-8">
     <meta name="author" content="Erik Partridge">
     <meta name="description" content="A Simulation of the Airline Industry">
-    {{ HTML::style("css/main.css") }}
+    {{ HTML::style("css/framework/style.css") }}
 
     <title>Stormy Skies Simulation</title>
 
 </head>
 <body>
     <noscript><span style="color: darkred; font-weight: bold;">WARNING!</span>JavaScript is needed for this website!</noscript>
-
-    <div id="main-header">
-        <a href="<?php echo url('views/hello.php', $parameters = array(), $secure = null);?>" title="Join!" ></a>
-    </div>
-
-    <div class="snippets">
-        <p>This is some text- this should be avenir</p>
-    </div>
-
-    <div class ="worlds">
-
-    </div>
+    <img src={{asset('images/core/main.jpg')}} alt="Plane in a stormy sky" width="100%" style="opacity: 1.0">
 </body>
 
 </html>
