3449c875449d5def2fbb65611718adbd7caa8464
Erik Malmstrom-Partridge <erazor101@gmail.com>
2014-11-12 13:20:31 -0500

    Algorithms

---
+++ laravel/app/views/emails/welcome.blade.php
@@ -0,0 +1,13 @@
+Just a little note to say:
+
+Welcome to TakeoffSim's mailing list! I'm glad to have you on board.
+
+TakeoffSim will be releasing shortly, but until then please don't hesitate to contact me at erikdevelopments@gmail.com, I'd love to hear your suggestions.
+
+I'd also like to encourage you to check out our forums at forums.takeoffsim.com, I recently posted some new specifications for TakeoffSim.
+
+Lastly, if you are interested in getting early access to TakeoffSim, please fill out this survey: https://docs.google.com/forms/d/1ZcuMIiYhv29SgvKy3zGPjFZ5MfN33LmQLT0sxi8jnS4/viewform?usp=send_form
+
+Have a great day,
+
+Erik Malmstrom-Partridge, CEO TakeoffSim.com
